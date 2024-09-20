7ae275e97181c5aea5e075e901488a1d6242689d
Buzhak <Byzhak@ya.ru>
2024-09-20 16:38:49 +0300

    create test

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # GitHub-API
-тестовое задание
+
+Тест для проверки работовспособности Github-API.
+Я использовал python библиотеку requests.
+Для скрытых данных используется библиотека dotenv.
+
+### Как работает тест?
+очень просто...
+
+1. Создаётся репозиторий на Github
+2. Проверка создался ли репозиторий
+3. Удаления репозитория с Github
+4. Проверка удалился ли репозиторий
+
+вся информация выводится в консоль.
+
+## Запуск теста
+
+1. Копируем репозиторий
+```
+git clone git@github.com:Buzhak/GitHub-API.git
+```
+
+2. Переходим в папку
+```
+cd GitHub-API
+```
+
+3. Создаём виртуальное окружение
+```
+python3 -m venv env
+```
+
+4. Запускаем виртуальное окружение
+win
+```
+env\Scripts\activate
+```
+linux\mac
+```
+source env/bin/activate
+```
+
+5. устанавливаем зависимости
+```
+pip install -r requirements.txt
+```
+
+6. Создаем файлик .env и заполняем его по примеру .env.example (это важный пункт, без этого ничего работать не будет)
+```
+git_token="ваш токен без кавычек"
+user_name="имя пользователя без кавычек"
+```
+7. Запускаем тест:
+```
+python test_api.py
+```
+
+## Пример корректоного прохождения теста
+
+```
+Repository "my_test_repo" created successfully!
+A repository "my_test_repo" exists.
+Repository "my_test_repo" deleted successfully!
+A repository "my_test_repo" does not exist.
+```
+
+## Технологии
+
+* python
+* requests
+* python-dotenv
+* GitHub-API
---
+++ test_api.py
@@ -0,0 +1,104 @@
+import os
+import json
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+token = os.getenv('git_token')
+user_name = os.getenv('user_name')
+base_url = 'https://api.github.com'
+test_repo_name = 'my_test_repo'
+
+headers = {
+    'Authorization': f'token {token}',
+    'Accept': 'application/vnd.github.v3+json'
+}
+
+
+def get_all_repos() -> list[str]:
+    '''
+    Функция получает все репозитории пользователя на Github
+    и возвращает список с названиями репозиториев
+    '''
+
+    url = f'{base_url}/users/{user_name}/repos'
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        repos = response.json()
+        repos = list(map(lambda repo: repo['name'], repos))
+        return repos
+    else:
+        print(f'Failed to fetch data: {response.status_code}, {response.text}')
+
+
+def is_repo_exists(repo_name: str) -> None:
+    '''
+    Функция принимает название репозитория в виде строки
+    и проверяет его наличие в общем списке репозиториев пользователя
+
+    repo_name: str
+    '''
+    all_repos = get_all_repos()
+
+    if repo_name in all_repos:
+        print(f'A repository "{repo_name}" exists.')
+    else:
+        print(f'A repository "{repo_name}" does not exist.')
+
+
+def create_new_repo(repo_name: str) -> None:
+    '''
+    Функция принимает название репозитория в виде строки
+    и создаёт репозиторий на Github
+
+    repo_name: str
+    '''
+
+    url = f'{base_url}/user/repos'
+    data = {
+        'name': repo_name,
+        'description': 'Test repo',
+        'private': False,
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    if response.status_code == 201:
+        print(f'Repository "{repo_name}" created successfully!')
+    else:
+        print(
+            f'Failed to create repository: {response.status_code}, '
+            f'{response.text}'
+        )
+
+
+def delete_repo_by_name(repo_name: str) -> None:
+
+    '''
+    Функция принимает название репозитория в виде строки
+    и удаляет репозиторий на Github
+
+    repo_name: str
+    '''
+
+    url = f'{base_url}/repos/{user_name}/{repo_name}'
+    response = requests.delete(url, headers=headers)
+
+    if response.status_code == 204:
+        print(f'Repository "{repo_name}" deleted successfully!')
+    else:
+        print(
+            f'Failed to delete repository: {response.status_code}, '
+            f'{response.text}'
+        )
+
+
+if __name__ == '__main__':
+    create_new_repo(test_repo_name)
+    is_repo_exists(test_repo_name)
+    delete_repo_by_name(test_repo_name)
+    is_repo_exists(test_repo_name)
